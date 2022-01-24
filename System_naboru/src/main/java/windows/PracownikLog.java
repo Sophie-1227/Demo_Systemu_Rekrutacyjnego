e048cec931cb9169a179ff8579918969b472d04d
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2022-01-24 01:00:33 +0100

    Added button for pracownikLog

--- System_naboru/src/main/java/windows/PracownikLog.java
+++ System_naboru/src/main/java/windows/PracownikLog.java
@@ -1,7 +1,11 @@
 package windows;
 
+import main.java.SearchEnginePracownika;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -62,6 +66,24 @@ public class PracownikLog {
         gbc.gridy = 1;
         JTextField pass = new JTextField("Password");
         panel.add(pass, gbc);
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.ipady = 20;
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        JButton enter = new JButton("Zaloguj");
+        panel.add(enter, gbc);
+
+        enter.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        SearchEnginePracownika sep = new SearchEnginePracownika();
+                        sep.setGridBagLayout();
+                        pracownikLogFrame.setVisible(false);
+                    }
+                }
+        );
 
         pracownikLogPanel.add(panel);
         pracownikLogFrame.setVisible(true);
